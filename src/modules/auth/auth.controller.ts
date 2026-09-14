0a28e5c9220f60578e624704b0fdc12b56b9ca9a
Source-Code-007 <utsho926@gmail.com>
2026-09-14 15:12:09 +0600

    sameSite to none for cookie resolve

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -19,7 +19,7 @@ export const login = catchAsync(async (req: Request, res: Response) => {
   res.cookie('token', result.token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
